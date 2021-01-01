4bedb5af943414681e66a44d77738d67d862014c
Kevyn Rocha <kevyn_oliver@hotmail.com>
2021-01-01 04:25:42 -0300

    feat(middleware): create middleware of validation routes

---
+++ src/middlewares/validation.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+import { validationResult } from 'express-validator';
+
+interface Error {
+  [key: string]: string;
+}
+
+const validate = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Response | void => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  const extractedErrors: Error[] = [];
+  errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }));
+
+  return res.status(422).json({
+    errors: extractedErrors,
+  });
+};
+
+export default validate;
