07fc43c4fd2dce36c89faceb0bc5d681ef1d2640
Kevyn Rocha <kevyn_oliver@hotmail.com>
2021-01-04 23:34:44 -0300

    refactor(controller): change database sessions controller and create store method

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
 
-import users from '../database/users';
+import User from '../models/user.model';
 
 const index = async (_: Request, res: Response): Promise<Response> => {
   try {
+    const users = await User.findAll();
+
     const formattedUsers = users.map(user => ({
       id: user.id,
       email: user.email
@@ -20,6 +22,38 @@ const index = async (_: Request, res: Response): Promise<Response> => {
   }
 };
 
+const store = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existUser = await User.findOne({ where: { email } });
+
+    if (existUser) {
+      throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+      email,
+      name,
+      password
+    });
+
+    const formattedUser = {
+      id: user.id,
+      name: user.name,
+      email: user.email
+    };
+
+    return res.json(formattedUser);
+  } catch (error) {
+    return res.status(400).json({
+      status: 400,
+      message: error.message
+    });
+  }
+};
+
 export default {
-  index
+  index,
+  store
 };
