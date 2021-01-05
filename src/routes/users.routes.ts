731acb9c84eb06d1ee1f90bcb92306bccde307be
Kevyn Rocha <kevyn_oliver@hotmail.com>
2021-01-04 23:35:41 -0300

    refactor(routes-user): create post route with validations

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,9 +2,17 @@ import { Router } from 'express';
 
 import UserController from '../controllers/users.controller';
 import authMiddleware from '../middlewares/auth';
+import validationMiddleware from '../middlewares/validation';
+import usersValidationRules from '../validations/users.validations';
 
 const router = Router();
 
 router.get('/', authMiddleware, UserController.index);
+router.post(
+  '/',
+  usersValidationRules,
+  validationMiddleware,
+  UserController.store
+);
 
 export default router;
